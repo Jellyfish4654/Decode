25b3a4b2db9c7d5ab1374e6dc790a0272077b3de
erkyyy <154486166+erkyyy@users.noreply.github.com>
2025-11-14 15:12:27 -0500

    jocelyn is cadding

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Hardware/Drivetrain.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.Framework.Hardware;
+
+import static android.icu.util.UniversalTimeScale.MAX_SCALE;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
+public class Drivetrain {
+
+    DcMotor[] driveMotors;
+    private VoltageSensor voltageSensor;
+
+    public Drivetrain(DcMotor[] dcMotors) {
+        this.driveMotors = dcMotors;
+    }
+
+    public void setMotorDirections(DcMotorSimple.Direction[] directions) {
+        for (int i = 0; i < driveMotors.length; i++) {
+            driveMotors[i].setDirection(directions[i]);
+        }
+    }
+
+    private double findMaxPower(double[] powers) {
+        double max = 0;
+        for (double power : powers) {
+            max = Math.max(max, Math.abs(power));
+        }
+        return max;
+    }
+    private void applyPrecisionAndScale(double multiplier, double[] powers) {
+        for (int i = 0; i < powers.length; i++) {
+            powers[i] *= multiplier;
+        }
+
+        double maxPower = findMaxPower(powers);
+        double scale = maxPower > MAX_SCALE ? MAX_SCALE / maxPower : 1.0;
+
+        for (int i = 0; i < powers.length; i++) {
+            powers[i] *= scale;
+        }
+    }
+
+    public void setMotorSpeeds(double multiplier, double[] powers) {
+        applyPrecisionAndScale(multiplier, powers);
+        for (int i = 0; i < driveMotors.length; i++) {
+            //applyVoltageCompensation(driveMotors[i], powers[i]);
+            driveMotors[i].setPower(powers[i]);
+        }
+    }
+//    public double applyVoltageCompensation(DcMotor motor, double power) {
+//        double voltageCompensation = 13.2/voltageSensor.getVoltage();
+//        power *= voltageCompensation;
+//        return power;
+//    }
+
+}
+
+// Cynthia was here and wrote this line of code ;)
+//I deleted Cynthia's line of code whoops
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Hardware/Outtake.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Framework.Hardware;
+
+public class Outtake {
+}
