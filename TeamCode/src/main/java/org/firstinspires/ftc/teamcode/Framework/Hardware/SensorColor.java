7992e65755115524f0d03b74ac544039d8dfe446
erkyyy <154486166+erkyyy@users.noreply.github.com>
2025-11-21 16:06:38 -0500

    Update SensorColor.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Hardware/SensorColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Hardware/SensorColor.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.Framework.Hardware;
 
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
 public class SensorColor {
     private final RevColorSensorV3 colorSensor;
@@ -10,16 +12,9 @@ public class SensorColor {
         colorSensor = sensor;
     }
 
-    public int[] detectColors () {
-        colorSensor.enableLed(true);
-        int red = colorSensor.red();
-        int blue = colorSensor.blue();
-        int green = colorSensor.green();
-        int alpha = colorSensor.alpha();
-
-        int combined = colorSensor.argb();
-
-        return new int[] {red, blue, green, alpha, combined};
+    public NormalizedRGBA detectColors () {
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
+        return colors;
     }
 
 }
