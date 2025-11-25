4f3314562b9f0908fd7fff6cdb26a31913545c16
erkyyy <154486166+erkyyy@users.noreply.github.com>
2025-11-25 15:50:20 -0500

    Create Controller.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Hardware/Controller.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Framework.Hardware;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+
+public class Controller {
+    private final GamepadEx Gamepad;
+    //finish this later
+}
