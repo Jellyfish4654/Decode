577a1f508abc9211d112ed64e703780e96d1bbce
erkyyy <154486166+erkyyy@users.noreply.github.com>
2025-11-28 12:46:02 -0500

    Update ColorTuner.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuners/ColorTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuners/ColorTuner.java
@@ -4,7 +4,6 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 
