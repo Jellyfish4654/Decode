fdc4820b2df254692883008a913c960bf6d7f555
erkyyy <154486166+erkyyy@users.noreply.github.com>
2025-11-28 12:45:45 -0500

    Create ColorTuner.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuners/ColorTuner.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Tuners;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+
+import org.firstinspires.ftc.teamcode.Framework.Hardware.SensorColor;
+
+@TeleOp(name = "Color Tuner", group = "Test")
+public class ColorTuner extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        final RevColorSensorV3 sensor;
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        sensor = hardwareMap.get(RevColorSensorV3.class, "colorSensor");
+
+        SensorColor color = new SensorColor(sensor);
+
+        double power;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("colors:", color.detectColors());
+            telemetry.update();
+        }
+    }
+}
