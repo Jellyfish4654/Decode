21e663c93606d64dd94682fb52c58fd7dd960adb
noahsausen <noahsausen@gmail.com>
2025-11-30 15:24:46 -0500

    fix auto imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Auto/tuning/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Auto/tuning/SplineTest.java
@@ -5,8 +5,8 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.TankDrive;
+import org.firstinspires.ftc.teamcode.Framework.Auto.MecanumDrive;
+import org.firstinspires.ftc.teamcode.Framework.Auto.TankDrive;
 
 public final class SplineTest extends LinearOpMode {
     @Override
