12172ebd6d7f3bfb45e5cffd48ea48370f475fd2
tac0coder <tacojulian@outlook.com>
2025-12-04 16:45:26 -0500

    I wanted to make a merge conflict for erk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyTele.java
@@ -10,6 +10,12 @@ public class JellyTele extends BaseOpMode {
     private final double PRECISION_MULTIPLIER_HIGH = 0.2;
     private final double DEADBAND_VALUE = 0.02;
     private final double STRAFE_ADJUSTMENT_FACTOR = (14.0 / 13.0);
+    private double SPINDEXER_DELAY = 0.55*1000;
+
+    private boolean isOuttakingGreen = false;
+    private boolean isOuttakingPurple = false;
+    private double spindexerStartTime = 0;
+
     @Override
     public void runOpMode() throws InterruptedException {
         initHardware();
@@ -17,6 +23,44 @@ public class JellyTele extends BaseOpMode {
         while (opModeIsActive()) {
             updateDrive(calculatePrecisionMultiplier());
             telemetry.update();
+        }
+    }
+
+    private void updateOuttake(){
+        if(!isOuttakingGreen && !isOuttakingPurple) {
+            if (controller.outGreenPressed()) {
+                outGreen();
+            } else if (controller.outPurplePressed()) {
+                outPurple();
+            }
+        }else{
+            if(System.currentTimeMillis()-spindexerStartTime >= SPINDEXER_DELAY){
+                outtake.outtakeOn();
+                isOuttakingGreen = false;
+                isOuttakingPurple = false;
+            }
+        }
+    }
+
+    private void outGreen(){
+        for(int slot = 1; slot <= 3; slot++){
+            if(spindexer.getContents(slot)==1){
+                spindexer.setSlot(-slot);
+                spindexerStartTime = System.currentTimeMillis();
+                isOuttakingGreen = true;
+                break;
+            }
+        }
+    }
+
+    private void outPurple(){
+        for(int slot = 1; slot <= 3; slot++){
+            if(spindexer.getContents(slot)==2){
+                spindexer.setSlot(-slot);
+                spindexerStartTime = System.currentTimeMillis();
+                isOuttakingGreen = true;
+                break;
+            }
         }
     }
 
