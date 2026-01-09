3b25c2d0e1bcd33bae13114c33c35c8bb0cd853b
erkyyy <154486166+erkyyy@users.noreply.github.com>
2026-01-08 22:31:23 -0500

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Auto/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Auto/MecanumDrive.java
@@ -70,7 +70,7 @@ public final class MecanumDrive {
         public RevHubOrientationOnRobot.LogoFacingDirection logoFacingDirection =
                 RevHubOrientationOnRobot.LogoFacingDirection.UP;
         public RevHubOrientationOnRobot.UsbFacingDirection usbFacingDirection =
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT;
 
         // drive model parameters
         public double inPerTick = 1;
