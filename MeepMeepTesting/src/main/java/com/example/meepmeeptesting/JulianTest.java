4a6f48ee3571cacd8e5f0e04f28ac8721ec21e96
tac0coder <tacojulian@outlook.com>
2026-01-11 18:10:01 -0500

    red auto redemption

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/JulianTest.java
@@ -0,0 +1,65 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.util.Vector;
+
+
+public class JulianTest {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity aud = new DefaultBotBuilder(meepMeep)
+                .setDimensions(18,18)
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(50, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        /*aud.runAction(aud.getDrive().actionBuilder(new Pose2d(-47, -47, Math.toRadians(-125)))
+                        .strafeToLinearHeading(new Vector2d(-12,-12),Math.toRadians(-125))
+                        .waitSeconds(1)
+                        .strafeToLinearHeading(new Vector2d(-12,-30),Math.toRadians(-90))
+                        .lineToY(-35)
+                        .waitSeconds(0.5)
+                        .lineToY(-40)
+                        .waitSeconds(0.5)
+                        .lineToY(-45)
+                        .splineToLinearHeading(new Pose2d(new Vector2d(1.5,-51),Math.toRadians(-125)),Math.toRadians(70))
+                        .splineToLinearHeading(new Pose2d(new Vector2d(-12,-19),Math.toRadians(-125)),Math.toRadians(-125))
+
+                .build());*/
+        aud.runAction(aud.getDrive().actionBuilder(new Pose2d(59, 12, Math.toRadians(70)))
+                        .waitSeconds(3)
+                .strafeToLinearHeading(new Vector2d(54,58), Math.toRadians(70))
+                        .waitSeconds(0.5)
+                .splineToConstantHeading(new Vector2d(60,58), Math.toRadians(90))
+                        .waitSeconds(0.5)
+                        .strafeToLinearHeading(new Vector2d(60,12),Math.toRadians(140))
+                        .waitSeconds(0.5)
+                        .splineToLinearHeading(new Pose2d(35.5,35,Math.toRadians(90)),Math.toRadians(90))
+                        .strafeToLinearHeading(new Vector2d(35.5,40),Math.toRadians(90))
+                .waitSeconds(0.5)
+                        .lineToYConstantHeading(45)
+                .waitSeconds(0.5)
+                        .strafeToLinearHeading(new Vector2d(60,12),Math.toRadians(140))
+                        .lineToX(30)
+                .build()
+        );
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(1f)
+                .addEntity(aud)
+                .start();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Auto/RedAudienceAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Auto/RedAudienceAuto.java
@@ -10,6 +10,7 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
@@ -25,7 +26,7 @@ public class RedAudienceAuto extends BaseOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         initHardware(true);
-        Pose2d initialPose = new Pose2d(61.5, 23.5, Math.toRadians(180));
+        Pose2d initialPose = new Pose2d(61.5, 23.5, Math.toRadians(70));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
         Params.alliance = Params.Alliance.RED;
 
@@ -38,6 +39,50 @@ public class RedAudienceAuto extends BaseOpMode {
         shootPose = null; //pos2
 
         TrajectoryActionBuilder moveToPark;
+
+
+        TrajectoryActionBuilder cornerOne = drive.actionBuilder(initialPose)
+                .strafeToLinearHeading(new Vector2d(54,58), Math.toRadians(70));
+        Pose2d cornerOnePose = new Pose2d(new Vector2d(54,58), Math.toRadians(70));
+
+        TrajectoryActionBuilder cornerTwo = drive.actionBuilder(cornerOnePose)
+                .splineToConstantHeading(new Vector2d(60,58), Math.toRadians(90));
+        Pose2d cornerTwoPose = new Pose2d(new Vector2d(60,58), Math.toRadians(90));
+
+        TrajectoryActionBuilder shootOne = drive.actionBuilder(cornerTwoPose)
+                .strafeToLinearHeading(new Vector2d(60,12),Math.toRadians(140));
+        Pose2d shootOnePose = new Pose2d(new Vector2d(60,12),Math.toRadians(140));
+
+        SequentialAction motifOneCollector = new SequentialAction(
+                new ParallelAction(
+                        spindexer.slotIn(),
+                        intake.intakeOn(),
+                        drive.actionBuilder(shootOnePose)
+                                .splineToLinearHeading(new Pose2d(35.5,35,Math.toRadians(90)),Math.toRadians(90))
+                                .build()
+                ),
+                new ParallelAction(
+                        spindexer.slotIn(),
+                        drive.actionBuilder(new Pose2d(35.5,35,Math.toRadians(90)))
+                                .strafeToLinearHeading(new Vector2d(35.5,40),Math.toRadians(90))
+                                .build()
+                ),
+                new ParallelAction(
+                        spindexer.slotIn(),
+                        drive.actionBuilder(new Pose2d(35.5,40,Math.toRadians(90)))
+                                .lineToYConstantHeading(45)
+                                .build()
+                ),
+                intake.intakeOff()
+        );
+
+        TrajectoryActionBuilder shootTwo = drive.actionBuilder(new Pose2d(35.5,45,Math.toRadians(90)))
+                .strafeToLinearHeading(new Vector2d(60,12),Math.toRadians(140));
+        Pose2d shootTwoPose = shootOnePose;
+
+        TrajectoryActionBuilder park = drive.actionBuilder(shootTwoPose)
+                .lineToX(30);
+
 
         // ↓ -------------- ↓ -------------- ↓ SHOOTING ACTIONS ↓ -------------- ↓ -------------- ↓
         SequentialAction swingPaddle = new SequentialAction(
@@ -84,27 +129,66 @@ public class RedAudienceAuto extends BaseOpMode {
                 outtake.outtakeOff()
         );
 
+
+        class ShootMotif implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                switch (Params.motif) {
+                    case GPP:
+                        Actions.runBlocking(
+                                shootGPP
+                        );
+                    case PGP:
+                        Actions.runBlocking(
+                                shootPGP
+                        );
+                    case PPG:
+                        Actions.runBlocking(
+                                shootPPG
+                        );
+
+                }
+                return true;
+            }
+        }
+
+
         // ↓ -------------- ↓ -------------- ↓ AUTO ↓ -------------- ↓ -------------- ↓
 
         waitForStart();
         if (!isStopRequested()) return;
         Actions.runBlocking(
-                new SequentialAction()
+                new SequentialAction(
+                        scanMotif(),
+                        new ShootMotif(),
+                        new ParallelAction(
+                                intake.intakeOn(),
+                                spindexer.slotIn(),
+                                cornerOne.build()
+                        ),
+                        new ParallelAction(
+                                spindexer.slotIn(),
+                                cornerTwo.build()
+                        ),
+                        shootOne.build(),
+                        new ParallelAction(
+                                spindexer.purpleOut(),
+                                outtake.outtakeOnFar()
+                        ),
+                        swingPaddle,
+                        new ParallelAction(
+                                spindexer.greenOut(),
+                                outtake.outtakeOnFar()
+                        ),
+                        swingPaddle,
+                        outtake.outtakeOff(),
+                        motifOneCollector,
+                        shootTwo.build(),
+                        new ShootMotif(),
+                        park.build()
+                )
         );
-        switch (Params.motif) {
-            case GPP:
-                Actions.runBlocking(
-                        shootGPP
-                );
-            case PGP:
-                Actions.runBlocking(
-                        shootPGP
-                );
-            case PPG:
-                Actions.runBlocking(
-                        shootPPG
-                );
-        }
+
 
     }
 
@@ -117,4 +201,6 @@ public class RedAudienceAuto extends BaseOpMode {
         }
     }
     public Action scanMotif() { return new ScanMotif(); }
+
+
 }
