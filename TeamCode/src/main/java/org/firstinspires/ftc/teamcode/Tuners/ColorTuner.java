b202837270f7b1c6ce0c2b6e8e27883b5f4e4d64
erkyyy <154486166+erkyyy@users.noreply.github.com>
2026-01-13 15:26:24 -0500

    hsv in colortuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuners/ColorTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tuners/ColorTuner.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Tuners;
 
+import android.graphics.Color;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -21,15 +23,22 @@ public class ColorTuner extends LinearOpMode
 
         SensorColor color = new SensorColor(sensor);
         double[] detectedColors;
+        double[] hsvValues;
 
         waitForStart();
 
         while (opModeIsActive()) {
             detectedColors = color.detectRGBA();
+            hsvValues = color.detectHSV();
+
             telemetry.addData("R", detectedColors[0]);
             telemetry.addData("G", detectedColors[1]);
             telemetry.addData("B", detectedColors[2]);
             telemetry.addData("A", detectedColors[3]);
+
+            telemetry.addData("H", hsvValues[0]);
+            telemetry.addData("S", hsvValues[1]);
+            telemetry.addData("V", hsvValues[2]);
 
             telemetry.addData("Is green?",color.isGreen());
             telemetry.addData("Is purple?",color.isPurple());
